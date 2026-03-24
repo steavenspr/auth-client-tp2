fcfe88e26234ac7a1cf5d9863278dd5ee6e454b3
Steavens <steavens2010@aol.com>
2026-03-25 01:55:34 +0400

    fix(sonar): remove sonar.java.libraries property to fix SonarCloud analysis error

---
+++ src/main/java/com/example/autclient/services/AuthServiceException.java
@@ -0,0 +1,8 @@
+package com.example.autclient.services;
+
+public class AuthServiceException extends Exception {
+    public AuthServiceException(String message) {
+        super(message);
+    }
+}
+
